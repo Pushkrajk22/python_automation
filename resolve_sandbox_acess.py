09a7a53731dc58dc31f649d12e6f5504535e32e0
Pushkraj Kulkarni <pushkraj.kulkarni@ibm.com>
2024-10-07 12:44:12 +0530

    Added resolve function with image

--- resolve_sandbox_acess.py
+++ resolve_sandbox_acess.py
@@ -2,11 +2,11 @@ import pyautogui as pg
 import time
 
 def locate_and_click(image_path):
-    time.sleep(0.2)
+    time.sleep(0.1)
     located_button = pg.locateCenterOnScreen(image_path, confidence=0.8)
-    pg.moveTo(located_button, duration=0.2)  # Move to the button smoothly over 1 second
+    pg.moveTo(located_button, duration=0.1)  # Move to the button smoothly over 1 second
     pg.click()  # Click the left mouse button
-    time.sleep(0.3)
+    time.sleep(0.2)
 
 def resolve_state():
     # Locate the center of the image on the screen
@@ -16,12 +16,12 @@ def resolve_state():
     if state_button is not None:
             pg.moveTo(state_button, duration=0.5)  # Move to the button smoothly over 1 second
             pg.click()  # Click the left mouse button
-            time.sleep(0.5)
+            time.sleep(0.2)
             resolved_button = pg.locateCenterOnScreen(r"C:\Users\PushkrajKulkarni\IDCP\Automation\images\resolved.png", confidence=0.8)
             pg.moveTo(resolved_button, duration=0.5)  # Move to the button smoothly over 1 second
             pg.click()  # Click the left mouse button
             #pg.hotkey("ctrl","v")
-            time.sleep(0.3)
+            time.sleep(0.2)
 
 def fill_resolution_info():
     time.sleep(0.2)
@@ -58,9 +58,14 @@ def fill_resolution_info():
     process_area_button = pg.locateCenterOnScreen(r"C:\Users\PushkrajKulkarni\IDCP\Automation\images\process_area.png", confidence=0.93)
     pg.moveTo(process_area_button, duration=0.2)  # Move to the button smoothly over 1 second
     pg.click()  # Click the left mouse button
-    time.sleep(0.3)
+    time.sleep(0.1)
     pg.press('A')
     pg.press('enter')
+
+
+def resolve_ticket():
+    locate_and_click(r"C:\Users\PushkrajKulkarni\IDCP\Automation\images\resolve.png")
+
 
 def add_customer_comments():
     locate_and_click(r"C:\Users\PushkrajKulkarni\IDCP\Automation\images\notes.png")
@@ -99,9 +104,9 @@ Thanks!"""
 if __name__ =="__main__":
     time.sleep(3)
     pg.scroll(1500)
-    time.sleep(0.5)
+    time.sleep(0.3)
     resolve_state()
     pg.scroll(-900)
     add_customer_comments()
     fill_resolution_info()
-    
+    resolve_ticket()
